2e756600f80fdcdcd9a95c518681d3016195dbb1
Nick T <netitov@gmail.com>
2020-08-08 17:49:09 +0300

    hotfix: review 1

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,18 @@ const popup = document.querySelector('.popup');
 const popupEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close-btn');
 
-const popupToggle = function (event) {
+let nameInput = popup.querySelector('.popup__text_name');
+let jobInput = popup.querySelector('.popup__text_occupation');
+let name = document.querySelector('.profile__name');
+let occupation = document.querySelector('.profile__occupation');
+
+const popupToggle = function () {
   popup.classList.toggle('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = occupation.textContent;
 }
+
+console.log(document.querySelector('.popup__text_name').value);
 
 const closePopup = function(event) {
   if (event.target !== event.currentTarget) return
@@ -20,11 +29,7 @@ popup.addEventListener('click', closePopup);
 let formElement = document.querySelector('.popup__container');
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let nameInput = popup.querySelector('.popup__text_name');
-    let jobInput = popup.querySelector('.popup__text_occupation');
 
-    let name = document.querySelector('.profile__name');
-    let occupation = document.querySelector('.profile__occupation');
     name.textContent = nameInput.value;
     occupation.textContent = jobInput.value;
     popupToggle();
