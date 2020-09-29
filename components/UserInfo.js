53bc313756d2d6fb922484069ef94f6627b18313
Nikita Titov <netitov@gmail.com>
2020-09-29 22:49:54 +0300

    feat: create class UserInfo

---
+++ components/UserInfo.js
@@ -0,0 +1,21 @@
+export default class UserInfo {
+  constructor ({ nameSelector, occupationSelector }) {
+    this._nameSelector = nameSelector;
+    this._occupationSelector = occupationSelector;
+
+  }
+
+  getUserInfo () {
+    this._ProfileValues = {};
+    this._ProfileValues.name = this._nameSelector.textContent;
+    this._ProfileValues.value = this._occupationSelector.textContent;
+
+    return this._ProfileValues;
+  }
+
+  setUserInfo (input) {
+    this._nameSelector = input.name;
+    this._occupationSelector = occupationSelector;
+  }
+
+}
