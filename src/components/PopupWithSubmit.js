d4ec9618fa31bffd1cfd0c587a22284cb358e2d8
Nikita Titov <netitov@gmail.com>
2020-10-12 20:30:45 +0300

    feat: add markup and class popup submit

---
+++ src/components/PopupWithSubmit.js
@@ -0,0 +1,19 @@
+import Popup from './Popup.js'
+
+export default class PopupWithSubmit extends Popup {
+  constructor (popupSelector, hanldeSubmitForm) {
+    super(popupSelector);
+    this._hanldeSubmitForm = hanldeSubmitForm;
+    this._formElement = this._popupSelector.querySelector('.popup__form');
+  }
+
+  setEventListeners () {
+    super.setEventListeners();
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+       this._hanldeSubmitForm();
+    })
+  }
+}
+
+
